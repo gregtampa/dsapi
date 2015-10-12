5f89e3d74e4268dbcc9d75a2726cb96756c92167
DEV247 <dev247.net@gmail.com>
2015-10-12 05:21:53 -0500

    starting project

---
+++ api-v1.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from json import dumps
+from sqlalchemy import create_engine
+
+db = create_engine('sqlite:///servers.db')
+
+app = Flask(__name__)
+api = Api(app)
+
+class AllServers(Resource):
+	def get(self):
+		conn = db.connect()
+		query = conn.execute("select server,port,type from servers")
+		return {'servers': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+
+class Servers(Resource):
+	def get(self):
+		conn = db.connect()
+		query = conn.execute("select server,port,type from servers")
+		return {'servers': [i[0] for i in query.cursor.fetchall()]}
+
+class Server(Resource):
+	def get(self, servername):
+		conn = db.connect()
+		query = conn.execute("select * from servers where server='%s'" % servername.lower())
+		result = {'server': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+		return result
+
+api.add_resource(AllServers, '/')
+api.add_resource(Server, '/<string:servername>','/<string:servername>/')
+api.add_resource(Servers, '/servers','/servers/')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
