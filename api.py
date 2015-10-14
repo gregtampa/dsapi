ab9869fa8c5519caf245959ea50f88b989867fcc
dev247 <dev247.net@gmail.com>
2015-10-14 04:07:19 -0500

    seperate uri for community and official, router uri for queries, work on manage uri

--- api.py
+++ api.py
@@ -4,20 +4,22 @@
 # https://github.com/miguelgrinberg/REST-tutorial/blob/master/rest-server-v2.py
 
 from flask import Flask, request, make_response, jsonify, abort
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask.ext.basicauth import BasicAuth
 import json
 from sqlalchemy import create_engine
 
 db = create_engine('sqlite:///servers.db')
-
 app = Flask(__name__)
 api = Api(app)
-app.config['BASIC_AUTH_USERNAME'] = 'john'
-app.config['BASIC_AUTH_PASSWORD'] = 'matrix'
+app.config['BASIC_AUTH_USERNAME'] = 'demo'
+app.config['BASIC_AUTH_PASSWORD'] = 'demo'
 basic_auth = BasicAuth(app)
 
-
+parser = reqparse.RequestParser(trim=True)
+parser.add_argument('server')
+parser.add_argument('port', type=int)
+parser.add_argument('type')
 
 # PUBLIC API QUERIES
 class All(Resource): # returns the full list of routers
@@ -40,9 +42,7 @@ class Community(Resource): # convert to return official or community ?
 		result = {'server': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
 		return result # return router information
 
-
-# PRIVATE/RESTRICTED API QUERIES
-class ManageServer(Resource): # router db manangement interface
+class Query(Resource): # router db manangement interface
 	@basic_auth.required
 	def get(self, servername):
 		conn = db.connect()
@@ -52,18 +52,40 @@ class ManageServer(Resource): # router db manangement interface
 			abort(404) # error is bad and you should feel bad, seriously fix this 
 		return result
 
-	# PUT (return '', 204)
-	# need to use flask_restful reqparse to validate post data
 
+# PRIVATE/RESTRICTED API QUERIES
+class Manage(Resource):
+	@basic_auth.required
+	def post(self): # we're adding a new router here
+		args = parser.parse_args(strict=True)
+		s,p,t = args['server'],args['port'],args['type']
 
-	# DELETE (return servername, 201)
+		print '%s : %d - %s' % ( s,p,t )
+		return '', 204
+
+	@basic_auth.required
+	def put(self): # we're updating a router here
+		# need to make sure that args['server'] exists in db and then update
+		args = parser.parse_args(strict=True)
+		s,p,t = args['server'],args['port'],args['type']
+		print '%s : %d - %s' % ( s,p,t )
+		return '', 204
+
+	@basic_auth.required
+	def delete(self): # we're removing a router here
+		args = parser.parse_args(strict=True)
+		s,p,t = args['server'],args['port'],args['type']
+		if s == 'None':
+			abort(404) # error is bad and you should feel bad, seriously fix this 
+		return s, 201
 
 
 
 api.add_resource(All, '/v1', '/v1/')
 api.add_resource(Official, '/v1/official', '/v1/official/')
 api.add_resource(Community, '/v1/community', '/v1/community/')
-api.add_resource(ManageServer, '/v1/manage/<string:servername>', endpoint='servername')
+api.add_resource(Query, '/v1/query/<string:servername>', endpoint='servername')
+api.add_resource(Manage, '/v1/manage', '/v1/manage/')
 
 if __name__ == '__main__':
 	app.run(debug=True)
